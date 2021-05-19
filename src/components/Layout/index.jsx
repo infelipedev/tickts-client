9e8b0c4563a6b6089328d8d2aa52fcf2d1a1c146
Felipe Gomes <infelipedev@protonmail.com>
2021-05-18 23:12:23 -0300

    💄 feat: darken background color for the page

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,12 +1,14 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Box, Container, Heading } from "@chakra-ui/react";
 
 export const Layout = ({ children, heading }) => {
   return (
-    <Container as="main" paddingY="8">
-      <Heading as="h1" marginBottom="8">
-        {heading}
-      </Heading>
-      {children}
-    </Container>
+    <Box backgroundColor="gray.100">
+      <Container as="main" paddingY="8">
+        <Heading as="h1" marginBottom="8">
+          {heading}
+        </Heading>
+        {children}
+      </Container>
+    </Box>
   );
 };
