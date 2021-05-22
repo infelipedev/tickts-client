a07849119fd7b324fb2a47bd92db8cfcac97c714
Felipe Gomes <infelipedev@protonmail.com>
2021-05-22 11:53:17 -0300

    ➖ refactor: remove Preact

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
-const withPreact = require("next-plugin-preact");
 
 const nextConfig = {
   future: { webpack5: true },
   images: { domains: ["bebemamae.com", "i0.wp.com"] },
 };
 
-module.exports = withBundleAnalyzer(withPreact(nextConfig));
+module.exports = withBundleAnalyzer(nextConfig);
